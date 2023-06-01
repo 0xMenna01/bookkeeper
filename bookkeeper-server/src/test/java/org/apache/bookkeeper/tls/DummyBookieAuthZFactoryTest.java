65a53c0f4564c675ca9430619c2138aa7f7b54c9
0xMenna01 <emanuele.cryptdev@gmail.com>
2023-06-01 19:44:57 +0200

    Dummy Mockito Test - Not working with BaDua

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/tls/DummyBookieAuthZFactoryTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/tls/DummyBookieAuthZFactoryTest.java
@@ -17,20 +17,52 @@
  */
 package org.apache.bookkeeper.tls;
 
+import static org.mockito.Mockito.when;
+
+import org.apache.bookkeeper.auth.BookieAuthProvider;
+import org.apache.bookkeeper.common.util.ReflectionUtils;
+import org.apache.bookkeeper.conf.ServerConfiguration;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.io.IOException;
 
 
 /**
  * Unit Tests for BookieAuthZFactory.
  */
-
+@RunWith(MockitoJUnitRunner.class)
 public class DummyBookieAuthZFactoryTest {
 
+    @InjectMocks
+    private BookieAuthZFactory bookieAuthZFactory;
+
+    @Mock
+    private ServerConfiguration mockedServerConfig = new ServerConfiguration();
+
+    @Before
+    public void setup() {
+
+        String factoryClassName = BookieAuthZFactory.class.getName();
+        bookieAuthZFactory = (BookieAuthZFactory) ReflectionUtils.newInstance(factoryClassName,
+            BookieAuthProvider.Factory.class);
+
+        when(mockedServerConfig.getAuthorizedRoles()).thenReturn(new String[] {"role1"});
+    }
 
     @Test
     public void testBookieAuthZInitSuccess() {
-        Assert.assertTrue(true);
+        try {
+            bookieAuthZFactory.init(mockedServerConfig);
+            Assert.assertTrue(true);
+        } catch (IOException e) {
+            Assert.fail("Supposed to initialize correctly due to mock");
+        }
     }
 }
 
